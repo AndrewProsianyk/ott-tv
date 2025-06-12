0329ba2defaec1436b151b2b4042238b6d4a12be
Andrii P <andrii@Andriis-MacBook-Pro.local>
2025-06-12 15:55:28 +0300

    delete trash

--- app/api/route.js
+++ app/api/route.js
@@ -1,32 +1,32 @@
-import { NextResponse } from "next/server";
-import { BASE_API_URL } from "../constants/apiRoutes";
+// import { NextResponse } from "next/server";
+// import { BASE_API_URL } from "../constants/apiRoutes";
 
-export async function GET() {
-  const accessToken = process.env.TMDB_API_READ_ACCESS_TOKEN;
+// export async function GET() {
+//   const accessToken = process.env.TMDB_API_READ_ACCESS_TOKEN;
 
-  try {
-    const response = await fetch(`${BASE_API_URL}/movie/popular`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    });
+//   try {
+//     const response = await fetch(`${BASE_API_URL}/movie/popular`, {
+//       method: "GET",
+//       headers: {
+//         Authorization: `Bearer ${accessToken}`,
+//         "Content-Type": "application/json",
+//       },
+//     });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(
-        `TMDb API Error: ${response.status} - ${
-          errorData.status_message || "Unknown error"
-        }`
-      );
-    }
+//     if (!response.ok) {
+//       const errorData = await response.json();
+//       throw new Error(
+//         `TMDb API Error: ${response.status} - ${
+//           errorData.status_message || "Unknown error"
+//         }`
+//       );
+//     }
 
-    const data = await response.json();
-    return NextResponse.json({
-      data,
-    });
-  } catch (error) {
-    console.error("Error fetching popular movies:", error);
-  }
-}
+//     const data = await response.json();
+//     return NextResponse.json({
+//       data,
+//     });
+//   } catch (error) {
+//     console.error("Error fetching popular movies:", error);
+//   }
+// }
